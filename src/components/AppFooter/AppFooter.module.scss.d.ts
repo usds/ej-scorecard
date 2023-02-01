9bbbd871980608ab0cbb9a91a7052ecda8b03c32
Vim USDS <vimal.k.shah@omb.eop.gov>
2023-02-01 15:14:21 -0800

    fix: runs lint on scss file

--- src/components/AppFooter/AppFooter.module.scss.d.ts
+++ src/components/AppFooter/AppFooter.module.scss.d.ts
@@ -1,12 +1,12 @@
 declare namespace AppFooterNamespace {
-    export interface IAppFooterScss {
-      appFooterContainer: string;
-    }
+  export interface IAppFooterScss {
+    appFooterContainer: string;
   }
+}
 
 declare const AppFooterScssModule: AppFooterNamespace.IAppFooterScss & {
-    /** WARNING: Only available when "css-loader" is used without "style-loader" or "mini-css-extract-plugin" */
-    locals: AppFooterNamespace.IAppFooterScss;
-  };
+  /** WARNING: Only available when "css-loader" is used without "style-loader" or "mini-css-extract-plugin" */
+  locals: AppFooterNamespace.IAppFooterScss;
+};
 
-  export = AppFooterScssModule;
+export = AppFooterScssModule;
